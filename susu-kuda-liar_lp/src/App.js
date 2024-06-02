4aa1e8c399b5136864bc9cfd607ceeaaefe6c26c
muhammadderic <muhammad.donny.e@gmail.com>
2024-06-02 20:20:21 +0700

    Create Hero component and styling it

--- susu-kuda-liar_lp/src/App.js
+++ susu-kuda-liar_lp/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 import "./styles/app.css";
 
 const App = () => {
   return (
     <>
-      <Navbar />
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
     </>
   )
 }
---
+++ susu-kuda-liar_lp/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import people from "../assets/people.png";
+import "../styles/header.css";
+
+const Header = () => {
+  return (
+    <header className="header section__padding">
+      <div className="header-content">
+        <h1 className="gradient__text">Susu Kuda Liar: <br />Natural Strength and Vitality</h1>
+        <p>Experience the invigorating power of nature with Susu Kuda Liar, where every sip delivers the natural strength and vitality of wild horse milk, crafted to energize your body and nourish your spirit</p>
+      </div>
+
+      <div className="header-content__input">
+        <input type="email" placeholder="Type your email" />
+        <button className="button">Get Started</button>
+      </div>
+
+      <div className="header-content__people">
+        <img src={people} alt="people want susu kuda liar" />
+        <p>In the last 24 hours, 1,600 people have requested Susu Kuda Liar</p>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
